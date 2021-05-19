f6c781b3ee08af3a16344d9f318578ba8c4c3898
dipikam-2204 <mahashabde.dipika@gmail.com>
2021-05-19 15:46:26 -0700

    adding property card and modal for the forms

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './View/logo.jpg';
+import React, {useState } from "react";
+import propertyImage from './View/propertyImage.jpeg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './View/App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
 
 function App() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div>
+    <div>
      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Nav className="mr-auto">
+    <Nav className="mr-auto"> 
     <Navbar bg="dark">
     <Navbar.Brand href="#home">
       <img
@@ -26,8 +37,46 @@ function App() {
         Heena Joshi
       </Nav.Link>
     </Nav>
-</Navbar>
+    </Navbar>
     </div>
+    <div>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={propertyImage} />
+      <Card.Body>
+      <Card.Title>1296 Milkweed Street</Card.Title>
+      <Card.Text>
+          Knock these forms out, before you knock on the door
+      </Card.Text>
+      <Button variant="primary" onClick={handleShow}>
+        Click Here
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </Card.Body>  
+      </Card>
+      </div>
+      </div>
   );
 }
 
