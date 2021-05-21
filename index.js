6da23df2cd6c718114bd6ebb7aadf8595371e30d
dipikam-2204 <mahashabde.dipika@gmail.com>
2021-05-21 12:59:28 -0700

    remove the cors

--- index.js
+++ index.js
@@ -11,25 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 
-// --> Add this
-// ** MIDDLEWARE ** //
-const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://heenajoshibhagwatapp.herokuapp.com']
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable")
-      callback(null, true)
-    } else {
-      console.log("Origin rejected")
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-app.use(helmet())
-// --> Add this
-app.use(cors(corsOptions))
-
 // Anything that doesn't match the above, send back the index.html file
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
